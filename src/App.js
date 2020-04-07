323445b4367566c6344ef6d889d037fc92922942
Leonardo leite <leonardo.leitemeira10@gmail.com>
2020-04-07 18:39:31 -0300

    Mudança feita no branch ExemploTeste

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ export default function App() {
   return (
     <>
       <h1>Mudança para usar de exemplo</h1>
+      <h2>Mudança feita no branch ExemploTeste</h2>
       <aside>
         <button onClick={() => setChoice(1)}>Stateless component</button>
         <button onClick={() => setChoice(2)}>Stateful component</button>
